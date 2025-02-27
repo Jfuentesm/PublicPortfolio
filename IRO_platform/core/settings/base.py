45396a3faf0e1cf57d9afb9b84d4ed03f109060b
Jfuentesm <juan.fuentes.martin@gmail.com>
2025-02-27 02:55:36 -0500

    .js file missing seems solved

--- IRO_platform/core/settings/base.py
+++ IRO_platform/core/settings/base.py
@@ -68,6 +68,7 @@ DATABASE_ROUTERS = (
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django_tenants.middleware.main.TenantMainMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',  # Authentication must run BEFORE our middleware
@@ -139,6 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR.parent, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
